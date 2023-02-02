8ecfe849280b27fde7c69a90ee930575151fb042
fortune <omorodion.fortune@gmail.com>
2023-02-02 23:25:33 +0100

    input field

--- my-project/src/components/Footer.tsx
+++ my-project/src/components/Footer.tsx
@@ -5,6 +5,9 @@ function Footer() {
     <footer className="bg-veryDarkBlue">
       <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
         <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+          <div className="mx-auto my-6 text-center text-white md:hidden">
+            Copyright &copy; 2023, All Rights Reserved
+          </div>
           <div>
             <img src="images/logo-white.svg" className="h-8" alt="" />
           </div>
@@ -13,7 +16,11 @@ function Footer() {
               <img src="images/icons8-facebook.svg" className="h-8" alt="" />
             </a>
             <a href="#">
-              <img src="images/icons8-youtube-logo.svg" className="h-8" alt="" />
+              <img
+                src="images/icons8-youtube-logo.svg"
+                className="h-8"
+                alt=""
+              />
             </a>
             <a href="#">
               <img src="images/icons8-twitter.svg" className="h-8" alt="" />
@@ -51,10 +58,25 @@ function Footer() {
             <a href="#" className="hover:text-brightRed">
               Privacy Policy
             </a>
-            
           </div>
         </div>
-
+        <div className="flex flex-col justify-between">
+          <form>
+            <div className="flex space-x-3">
+              <input
+                type="text"
+                className="flex-1 px-4 rounded-full focus:outline-none"
+                placeholder="Updated in your inbox"
+              />
+              <button className="px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedlight focus:outline-none">
+                Go
+              </button>
+            </div>
+          </form>
+          <div className="hidden text-white md:block">
+            Copyright &copy; 2023 All Rights Reserved
+          </div>
+        </div>
       </div>
     </footer>
   );
--- my-project/src/components/Header.tsx
+++ my-project/src/components/Header.tsx
@@ -31,6 +31,23 @@ function Header() {
           >
             Get Started
           </a>
+          <button
+            id="menu-btn"
+            className="block hamburger md:hidden focus:outline-none"
+          >
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </button>
+        </div>
+        <div className="md:hidden">
+          <div id="menu" className="absolute flex flex-col items-center self-end hidden py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
+            <a href="#">Pricing</a>
+            <a href="#">Product</a>
+            <a href="#">About Us</a>
+            <a href="#">Careers</a>
+            <a href="#">Community</a>
+          </div>
         </div>
       </nav>
     </div>
