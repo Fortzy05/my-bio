75a140379ab4c9d382a935c8072748cbb739ad63
fortune <omorodion.fortune@gmail.com>
2023-02-02 21:34:47 +0100

    footer styling

--- my-project/src/components/Footer.tsx
+++ my-project/src/components/Footer.tsx
@@ -2,11 +2,61 @@ import React from "react";
 
 function Footer() {
   return (
-    <section>
-      <div className="items-center bg-black">
-        
+    <footer className="bg-veryDarkBlue">
+      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
+        <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+          <div>
+            <img src="images/logo-white.svg" className="h-8" alt="" />
+          </div>
+          <div className="flex justify-center space-x-4">
+            <a href="#">
+              <img src="images/icon-facebook.svg" className="h-8" alt="" />
+            </a>
+            <a href="#">
+              <img src="images/icon-youtube.svg" className="h-8" alt="" />
+            </a>
+            <a href="#">
+              <img src="images/icon-twitter.svg" className="h-8" alt="" />
+            </a>
+            <a href="#">
+              <img src="images/icon-pintrest.svg" className="h-8" alt="" />
+            </a>
+            <a href="#">
+              <img src="images/icon-instagram.svg" className="h-8" alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="flex justify-around space-x-32">
+          <div className="flex flex-col space-y-3 text-white">
+            <a href="#" className="hover:text-brightRed">
+              Home
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              Pricing
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              Product
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              About
+            </a>
+          </div>
+          <div className="flex flex-col space-y-3 text-white">
+            <a href="#" className="hover:text-brightRed">
+              Careers
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              Community
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              Privacy Policy
+            </a>
+            
+          </div>
+        </div>
+
       </div>
-    </section>
+    </footer>
   );
 }
 
