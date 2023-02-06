6424bbfe81a888f48a2f853aada0438301aed27e
fortune <omorodion.fortune@gmail.com>
2023-02-06 16:51:03 +0100

    hamburger menu

--- my-project/src/components/Header.tsx
+++ my-project/src/components/Header.tsx
@@ -1,56 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
   return (
-    <div className="">
-      <nav className="relative container p-6 mx-auto">
-        <div className="flex items-center justify-between">
-          <div className="pt-2">
-            <img src="images/logo.svg" alt="" />
-          </div>
-          <div className="hidden md:flex space-x-6 font-bold">
-            <a href="#" className="hover:text-darkGrayishBlue">
-              Pricing
-            </a>
-            <a href="#" className="hover:text-darkGrayishBlue">
-              Product
-            </a>
-            <a href="#" className="hover:text-darkGrayishBlue">
-              About Us
-            </a>
-            <a href="#" className="hover:text-darkGrayishBlue">
-              Careers
-            </a>
-            <a href="#" className="hover:text-darkGrayishBlue">
-              Community
-            </a>
-          </div>
-          <a
-            href="#"
-            className="hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
-          >
-            Get Started
+    <nav className="relative container p-6 mx-auto">
+      <div className="flex items-center justify-between">
+        <div className="pt-2">
+          <img src="images/logo.svg" alt="" />
+        </div>
+        <div className="hidden md:flex space-x-6 font-bold">
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Pricing
           </a>
-          <button
-            id="menu-btn"
-            className="block hamburger md:hidden focus:outline-none"
-          >
-            <span className="hamburger-top"></span>
-            <span className="hamburger-middle"></span>
-            <span className="hamburger-bottom"></span>
-          </button>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Product
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            About Us
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Careers
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Community
+          </a>
         </div>
-        <div className="md:hidden">
-          <div id="menu" className="absolute flex flex-col items-center self-end hidden py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
-            <a href="#">Pricing</a>
-            <a href="#">Product</a>
-            <a href="#">About Us</a>
-            <a href="#">Careers</a>
-            <a href="#">Community</a>
-          </div>
+        <a
+          href="#"
+          className="hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+        >
+          Get Started
+        </a>
+        <button
+          id="menu-btn"
+          className=" block hamburger md:hidden focus:outline-none"
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
+      </div>
+      <div className="md:hidden">
+        <div
+          id="menu"
+          className="absolute flex-col items-center self-end hidden py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md"
+        >
+          <a href="#">Pricing</a>
+          <a href="#">Product</a>
+          <a href="#">About Us</a>
+          <a href="#">Careers</a>
+          <a href="#">Community</a>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
